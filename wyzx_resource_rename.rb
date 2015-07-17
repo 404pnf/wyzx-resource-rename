afe6ce1e1d4c8c59170bb49517b0e34c73d32f82
404pnf <404pnf@exmaple.org>
2015-07-17 10:29:46 +0800

    fix rubocop warnings

--- wyzx_resource_rename.rb
+++ wyzx_resource_rename.rb
@@ -48,9 +48,8 @@
 #
 # a.jpg, 3, 2, pic
 
-
 ## 实际csv举例
-
+#
 # Unit,Section,Sub-section,Task,Activity / Step,Question,原文件名,新文件名
 #
 # 1,Listening to the world (2),Sharing (1),1,,,U1_1_1.mp4,u1_2.1.1.mp4
@@ -62,25 +61,22 @@
 # ## 规则补充
 #
 # 说明： 1 基于新视野视听说的层级分析，Section和Sub-section对应的层级数字是固定的，具体可见各Section和Sub-section标题文字后的数字。
-#       2 如果文件名中出现某一个中间层级为空的情况，那么该层级的数字表现为0
-#       3 如果某层级有多个同级别的文件时，则用a、b等英文字母来区别
-
+#       1 如果文件名中出现某一个中间层级为空的情况，那么该层级的数字表现为0
+#       1 如果文件末尾连续出现空的层级，在新文件名中忽略它们
+#       1 如果某层级有多个同级别的文件时，则用a、b等英文字母来区别
 
 require 'csv'
 require 'FileUtils'
-#require 'did_you_mean'
+# require 'did_you_mean'
 
 # namespace
 module WyzxRename
-  debug = true
-
   module_function
 
   # >> db.headers
   # => [:unit, :section, :subsection, :task, :activity_step, :question, :orig_filename, :new_filename]
   def go(h, in_dir, out_dir)
     # 清理csv中的字符串
-    # 删除收尾多余空格，转换nil，判断去除多余空格后是否为空
     h = h.each_with_object({}) do |(k, v), a|
       if k == :orig_filename  # 原始文件名不应该改变，否则它也变成只剩数字了
         a[k] = v.strip
@@ -97,27 +93,20 @@ module WyzxRename
     orig_filename = h[:orig_filename]
     suffix = File.extname(orig_filename)
 
-    # new_filename规则
-    # 如果文件名中出现某一个中间层级为空的情况，那么该层级的数字表现为0
+    # 根据规则拼出新的文件名
     new_fn_arr = [unit, section, subsection, task, activity_step, question]
-    # 去掉末尾连续出现的空白。
+    # 如果文件末尾连续出现空的层级，在新文件名中忽略它们
     # 借用数组的drop_while
     # >> a = [ ' ', ' ', 'ab']
     # => [" ", " ", "ab"]
     # >> a.drop_while { |e| e == ' ' }
     # => ["ab"]
-    new_fn_without_suffix = new_fn_arr.reverse
-      .drop_while { |e| e == ' '}
-      .reverse
-      .map { |e| e == ' ' ? 0 : e } # 某一个中间层级为空，补0
-      .join('_')
+    new_fn_without_suffix = assemble_new_filename(new_fn_arr)
     new_filename = "#{new_fn_without_suffix}#{suffix}"
     p new_filename
 
-
     orig_file_with_path = File.join in_dir, orig_filename
     new_file_with_path = File.join out_dir, new_filename
-
   end
 
   # 清理csv中的字符串
@@ -131,13 +120,21 @@ module WyzxRename
   # case C: csv内容有多余的注释内容 Listening to the world (2), 只保留数字
   # case D: 需要保留 3a 3b 这种的a和b
   def normalize_str(s)
-    s = s || ' '
-    ss = s.match(/\d[0-9a-z]?/).to_s
-    if ss.strip() == ''
-        ' '
+    s ||= ''
+    ss = s.match(/\d[0-9a-z]*/).to_s # 匹配 1, 12, 1a, 1abc
+    if ss.strip == ''
+      ' '
     else
-      ss.strip()
+      ss.strip
     end
+  end
+
+  def assemble_new_filename(arr)
+    arr.reverse
+      .drop_while { |e| e == ' ' }
+      .reverse
+      .map { |e| e == ' ' ? 0 : e } # 中间层级为空，补0
+      .join('_')
   end
 
   def mkdir_if_not_exist(newdir)
