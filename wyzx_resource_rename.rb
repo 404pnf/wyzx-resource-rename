78e6838baf874b96197218bd32c9d24f78210995
404pnf <404pnf@exmaple.org>
2015-07-17 10:14:34 +0800

    进展不错

--- wyzx_resource_rename.rb
+++ wyzx_resource_rename.rb
@@ -48,57 +48,109 @@
 #
 # a.jpg, 3, 2, pic
 
+
+## 实际csv举例
+
+# Unit,Section,Sub-section,Task,Activity / Step,Question,原文件名,新文件名
+#
+# 1,Listening to the world (2),Sharing (1),1,,,U1_1_1.mp4,u1_2.1.1.mp4
+#
+# 1,Listening to the world (2),Sharing (1),2,,,,
+#
+# 1,Listening to the world (2),Sharing (1),3,,,,
+
+# ## 规则补充
+#
+# 说明： 1 基于新视野视听说的层级分析，Section和Sub-section对应的层级数字是固定的，具体可见各Section和Sub-section标题文字后的数字。
+#       2 如果文件名中出现某一个中间层级为空的情况，那么该层级的数字表现为0
+#       3 如果某层级有多个同级别的文件时，则用a、b等英文字母来区别
+
+
 require 'csv'
 require 'FileUtils'
-require 'did_you_mean'
+#require 'did_you_mean'
 
 # namespace
 module WyzxRename
-  @debug = true
+  debug = true
 
   module_function
 
+  # >> db.headers
+  # => [:unit, :section, :subsection, :task, :activity_step, :question, :orig_filename, :new_filename]
   def go(h, in_dir, out_dir)
-    @in, @out = in_dir, out_dir
-    @filename = normalize_str h[:filename]
-    @book = normalize_str h[:book]
-    @unit = normalize_str h[:unit]
-    @type = normalize_str h[:type]
-    @newdir = assemble_new_dir
-    o_fn = File.join @in, @filename
-    n_fn = assemble_new_filename
-    copy_to_new_folder o_fn, File.join(@newdir, n_fn)
+    # 清理csv中的字符串
+    # 删除收尾多余空格，转换nil，判断去除多余空格后是否为空
+    h = h.each_with_object({}) do |(k, v), a|
+      if k == :orig_filename  # 原始文件名不应该改变，否则它也变成只剩数字了
+        a[k] = v.strip
+      else
+        a[k] = normalize_str(v)
+      end
+    end
+    unit =  h[:unit]
+    section = h[:section]
+    subsection =  h[:subsection]
+    task =  h[:task]
+    activity_step =  h[:activity_step]
+    question =  h[:question]
+    orig_filename = h[:orig_filename]
+    suffix = File.extname(orig_filename)
 
-    p @newdir if @debug
+    # new_filename规则
+    # 如果文件名中出现某一个中间层级为空的情况，那么该层级的数字表现为0
+    new_fn_arr = [unit, section, subsection, task, activity_step, question]
+    # 去掉末尾连续出现的空白。
+    # 借用数组的drop_while
+    # >> a = [ ' ', ' ', 'ab']
+    # => [" ", " ", "ab"]
+    # >> a.drop_while { |e| e == ' ' }
+    # => ["ab"]
+    new_fn_without_suffix = new_fn_arr.reverse
+      .drop_while { |e| e == ' '}
+      .reverse
+      .map { |e| e == ' ' ? 0 : e } # 某一个中间层级为空，补0
+      .join('_')
+    new_filename = "#{new_fn_without_suffix}#{suffix}"
+    p new_filename
+
+
+    orig_file_with_path = File.join in_dir, orig_filename
+    new_file_with_path = File.join out_dir, new_filename
+
   end
 
+  # 清理csv中的字符串
+  # 1. 删除收尾多余空格
+  # 2. 转换nil
+  # 3. 判断去除多余空格后是否为空
+  # 4. 从文本中捕获数字和3a这种文本
+  #
+  # case A: csv内容为空是，会解析为nil，将nil转为一个空格
+  # case B: csv内容只是一些空格，我们也应该先清除这些空格看看是否有实际内容。没有的话，将其设定为一个空格。
+  # case C: csv内容有多余的注释内容 Listening to the world (2), 只保留数字
+  # case D: 需要保留 3a 3b 这种的a和b
   def normalize_str(s)
-    s.strip.downcase
+    s = s || ' '
+    ss = s.match(/\d[0-9a-z]?/).to_s
+    if ss.strip() == ''
+        ' '
+    else
+      ss.strip()
+    end
   end
 
-  def assemble_new_dir
-    File.join @out, @type, "book_#{@book}", "unit_#{@unit}"
-  end
-
-  def assemble_new_filename
-    @filename
-  end
-
-  def normalize_filename
-    @filename
-  end
-
-  def mkdir_if_not_exist
-    FileUtils.mkdir_p @newdir unless File.exist? @newdir
+  def mkdir_if_not_exist(newdir)
+    FileUtils.mkdir_p newdir unless File.exist? newdir
   end
 
   def copy_to_new_folder(o, n)
-    mkdir_if_not_exist
     FileUtils.cp o, n
   end
 end
 
 def main(csv, in_dir, out_dir)
+  p "输入目录是 #{in_dir}。输出目录是 #{out_dir}"
   # 默认的converters: numeric 我们要的就是字符而不是数字
   CSV.table(csv, converters: nil).each do |e|
     WyzxRename.go e, in_dir, out_dir
